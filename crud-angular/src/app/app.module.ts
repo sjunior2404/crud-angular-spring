fdeaca4c4a8068679d11242dba13346705a85a9b
Sérgio Júnior <sergio_jfj@hotmail.com>
2025-04-25 17:53:56 -0300

    Criação Módulo de Cursos e Usando roteamento com Lazy Load
    Criação Módulo de Cursos e Usando roteamento com Lazy Load

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
