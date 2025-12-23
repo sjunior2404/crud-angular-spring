f15b78251b0265ce34e1bc6ccbf2673b7199b59a
Sérgio Júnior <sergio_jfj@hotmail.com>
2025-12-23 17:08:25 -0300

    Angular Typed Forms

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -20,7 +20,7 @@ export class CoursesService {
       tap(courses => console.log(courses))
     );
   }
-  save(record: Course):Observable<Course>{
+  save(record: Partial <Course>){
     return this.httpClient
     .post<Course>(this.API, record)
     .pipe(first())
